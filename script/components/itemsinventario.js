0b4283d9eeaa76e677b3b31e83d8af0c613b1795
KatriEsFa <katriesfa@gmail.com>
2022-05-03 17:55:34 -0300

    Se agregó fetch para los elementos del inventario

---
+++ script/components/itemsinventario.js
@@ -0,0 +1,23 @@
+
+window.addEventListener('DOMContentLoaded', showItems);
+function showItems() {
+    const itemsList = document.getElementById("itemsList");
+    fetch("/script/data/arrayitems.json")
+        .then((response) => response.json())
+        .then((data) => (
+
+            data.forEach(element => {
+                const li = document.createElement("li.item")
+                li.innerHTML =
+                    `
+                    <h3>${element.nombreItem}</h3>
+                    <h4>${element.tipo}</h4>
+                
+                `
+                itemsList.append(li)
+
+            })
+        )
+        )
+
+}
