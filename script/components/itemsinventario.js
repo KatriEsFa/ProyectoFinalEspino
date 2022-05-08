29fb5d079e2fa5168b8818a042fa3451646ba5e9
KatriEsFa <katriesfa@gmail.com>
2022-05-08 15:10:47 -0300

    Se arregló el conjunto de items prestables que hay y como se presentan

--- script/components/itemsinventario.js
+++ script/components/itemsinventario.js
@@ -1,22 +1,34 @@
 
 window.addEventListener('DOMContentLoaded', showItems);
 function showItems() {
-    const itemsList = document.getElementById("itemsList");
+    const availableItemsList = document.getElementById("availableItemsList");
+    const notAvailableItemsList = document.getElementById("notAvailableItemsList");
+
     fetch("/script/data/arrayitems.json")
         .then((response) => response.json())
         .then((data) => (
-
             data.forEach(element => {
-                const li = document.createElement("li");
-                li.classList.add("item")
-                li.innerHTML =
-                    `
+                if (element.prestamo === false) {
+                    const li = document.createElement("li");
+                    li.classList.add("availableItem")
+                    li.innerHTML =
+                        `
                     <h3>${element.nombreItem}</h3>
                     <h4>${element.tipo}</h4>
-                
-                `
-                itemsList.append(li)
-
+                    <input type="button" value="Solicitar"> 
+                        `
+                    availableItemsList.append(li)
+                } else {
+                    const li2 = document.createElement("li");
+                    li2.classList.add("unavailableItem")
+                    li2.innerHTML =
+                        `
+                    <h3>${element.nombreItem}</h3>
+                    <h4>${element.tipo}</h4>
+                    <h3>Elemento no diponible</h3> 
+                        `
+                    notAvailableItemsList.append(li2)
+                }
             })
         )
         )
